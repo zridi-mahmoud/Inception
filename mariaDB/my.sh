dd87931ce50718473934fa505259eec6a91db55a
zridi-mahmoud <mahmoud.zridi0919@gmail.com>
2023-11-06 00:55:37 +0100

    feat: ngnix dockerfile

--- mariaDB/my.sh
+++ mariaDB/my.sh
@@ -2,11 +2,11 @@
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i '10i user=root' /etc/mysql/mariadb.conf.d/50-server.cnf
-cat /etc/mysql/mariadb.conf.d/50-server.cnf
-service mariadb start
+# cat /etc/mysql/mariadb.conf.d/50-server.cnf
+service mariadb start && sleep 5
 mysql -u root -e "CREATE DATABASE $WORDPRESS_DB_NAME;"
 mysql -u root -e "CREATE USER '$MARIADB_USER_NAME'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$MARIADB_USER_NAME'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root -e "exit"
-service mariadb stop
+kill `cat /var/run/mysqld/mysqld.pid`
